559cf1a70376a7a1df52a5644b466cef8c509134
Matheus_Alexandre <alexandremhm@gmail.com>
2021-07-20 18:34:35 -0300

    Add trivia

---
+++ build/precache-manifest.e4f6dcfd3b858b5233d322a6a4d1ea8b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b48b941d0d8d85c6ec2278fc7fec6664",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "d091d72e095e59f32934",
+    "url": "/trivia/static/css/main.5b68fe12.chunk.css"
+  },
+  {
+    "revision": "5c8622636b73a19f69cb",
+    "url": "/trivia/static/js/2.3940a305.chunk.js"
+  },
+  {
+    "revision": "5d1b8067085602e259fd0862f57df793",
+    "url": "/trivia/static/js/2.3940a305.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d091d72e095e59f32934",
+    "url": "/trivia/static/js/main.2fc53561.chunk.js"
+  },
+  {
+    "revision": "23dbb679c736924e4b1a",
+    "url": "/trivia/static/js/runtime-main.6e763e45.js"
+  },
+  {
+    "revision": "245ec5e3ade2498c294c9689df7232b4",
+    "url": "/trivia/static/media/pablita-530.245ec5e3.png"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trivia/static/media/trivia.466d153e.png"
+  }
+]);
