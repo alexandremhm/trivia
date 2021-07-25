81d9919290327ab8f49ef84ce307aedf8214fb47
Matheus_Alexandre <alexandremhm@gmail.com>
2021-07-25 17:39:29 -0300

    Updates

---
+++ precache-manifest.144588ef3ddc2f61525931d845d7f76c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7aa4f95f47d4efad07e42c432021f81",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "a11a9af50de8cbdd1a34",
+    "url": "/trivia/static/css/main.1b28f3fe.chunk.css"
+  },
+  {
+    "revision": "28b0653ba08170c38ed0",
+    "url": "/trivia/static/js/2.0d78840c.chunk.js"
+  },
+  {
+    "revision": "5d1b8067085602e259fd0862f57df793",
+    "url": "/trivia/static/js/2.0d78840c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a11a9af50de8cbdd1a34",
+    "url": "/trivia/static/js/main.d11af8c4.chunk.js"
+  },
+  {
+    "revision": "23dbb679c736924e4b1a",
+    "url": "/trivia/static/js/runtime-main.6e763e45.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trivia/static/media/trivia.466d153e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/trivia/precache-manifest.632e007c02defc3ec74c16f5bbe2861d.js"
+  "/trivia/precache-manifest.144588ef3ddc2f61525931d845d7f76c.js"
 );
 
 self.addEventListener('message', (event) => {
